0594f2911e3c36cfaca13725060cc5719fa093ba
unknown <gianluca.imbiscuso@gmail.com>
2022-05-12 23:31:33 +0200

    Start the extractor using 'start.py'. For the moment it will give you two equally structured arrays containing 'UnitDescriptor' and 'WeaponDescriptor' samples. Next big step would be trying to export them to a .csv file for easy MySQL import.

---
+++ Extractor/start.py
@@ -0,0 +1,8 @@
+import UnitDescriptor
+import WeaponDescriptor
+
+#Extract NDF files
+print(str(UnitDescriptor.extract("data/UniteDescriptor.ndf")) + "\n")
+print(str(WeaponDescriptor.extract("data/WeaponDescriptor.ndf")) + "\n")
+
+input ("Press Enter to close...")
